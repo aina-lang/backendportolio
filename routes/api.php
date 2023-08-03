47be771422c43b5c777ace861e8991ad54f9c5e7
aina-lang <merciaaina@gmail.com>
2023-08-03 17:22:08 +0300

    Mettre à jour après l'installation du package npm

--- routes/api.php
+++ routes/api.php
@@ -14,4 +14,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::post('/contact', [ContactController::class, 'sendContactForm']);
+Route::post('/', [ContactController::class, 'sendContactForm']);
