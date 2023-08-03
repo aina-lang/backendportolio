9e7e776cd03fdab2d3e5a5c3c73e20c0a23643da
aina-lang <merciaaina@gmail.com>
2023-08-03 17:55:54 +0300

    Mettre à jour après l'installation du package npm

--- api/index.php
+++ api/index.php
@@ -1,2 +1,7 @@
 <?php
+
+header("Access-Control-Allow-Origin: https://64cbbf22c3451106af7fa7fc--dainty-moxie-28abff.netlify.app");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 require __DIR__ . "/../public/index.php";
