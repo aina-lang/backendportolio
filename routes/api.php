d28dc4f2ada01718c424f3104d01bfa24d0c6596
aina-lang <merciaaina@gmail.com>
2023-08-10 13:56:19 +0300

    commit

--- routes/api.php
+++ routes/api.php
@@ -5,4 +5,8 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MailController;
 
 
+header("Access-Control-Allow-Origin: https://ainamercia.netlify.app");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
 Route::post('/', [MailController::class, 'sendEmail']);
